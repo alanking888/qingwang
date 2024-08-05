0d0de8d3cefdf59da7480ea74c39856be2d1dcd9
alanking888 <64194984+alanking888@users.noreply.github.com>
2024-08-05 12:38:23 +0800

    Add files via upload

---
+++ js/test.js
@@ -0,0 +1,7 @@
+// var boolean=window.confirm("即将访问青网科技园活动了解页面")
+// if(boolean==true){
+//     var a=window.prompt("请问您贵姓？","")
+//     document.writeln(a+'先生(女士),欢迎参加本次活动!')
+// }
+// else{window.location.href = "https://www.bilibili.com";}
+   
